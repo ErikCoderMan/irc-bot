1320ddb4be1303fcde97eaffd4f03b0357e4206d
Erik A <196097247+ErikCoderMan@users.noreply.github.com>
2025-12-12 06:53:33 +0100

    added file for paths for convenience

---
+++ core/paths.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+# Project root
+ROOT_DIR = Path(__file__).parent.parent
+
+# Paths to key files
+CREDENTIALS_FILE = ROOT_DIR / "credentials.json"
+LOG_FILE = ROOT_DIR / "bot.log"
+NOTES_FILE = ROOT_DIR / "notes.json"
